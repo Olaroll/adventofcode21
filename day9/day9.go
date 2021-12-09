381c508bc6225e1fadf952dde728ceb82c7d81c1
Olari Tsernobrovkin <olaritserno@gmail.com>
2021-12-09 10:39:49 +0200

    Day 9 - Pretty happy with today's solution, but it took longer than I hoped

---
+++ day9/day9.go
@@ -0,0 +1,100 @@
+package day9
+
+import (
+	"github.com/Olaroll/adventofcode21/utils"
+	"sort"
+)
+
+type Board [][]byte
+
+func checkLow(board Board, x, y int) int {
+	if y > 0 && board[y-1][x] <= board[y][x] {
+		return 0
+	}
+	if y < len(board)-1 && board[y+1][x] <= board[y][x] {
+		return 0
+	}
+	if x > 0 && board[y][x-1] <= board[y][x] {
+		return 0
+	}
+	if x < len(board[y])-1 && board[y][x+1] <= board[y][x] {
+		return 0
+	}
+	return int(board[y][x] + 1)
+}
+
+func Solve1() int {
+	lines := utils.GetLines("./day9/input.txt")
+
+	board := make(Board, len(lines))
+	for y := range board {
+		board[y] = []byte(lines[y])
+		for x := range board[y] {
+			board[y][x] -= '0'
+		}
+	}
+
+	var sum int
+	for y := range board {
+		for x := range board[y] {
+			sum += checkLow(board, x, y)
+		}
+	}
+
+	return sum
+}
+
+func expand(counter *int, board Board, x, y int) bool {
+	*counter++
+	board[y][x] = 9
+
+	// Check UP
+	if y > 0 && board[y-1][x] != 9 {
+		expand(counter, board, x, y-1)
+	}
+
+	// Check DOWN
+	if y < len(board)-1 && board[y+1][x] != 9 {
+		expand(counter, board, x, y+1)
+	}
+
+	// Check LEFT
+	if x > 0 && board[y][x-1] != 9 {
+		expand(counter, board, x-1, y)
+	}
+
+	// Check RIGHT
+	if x < len(board[y])-1 && board[y][x+1] != 9 {
+		expand(counter, board, x+1, y)
+	}
+
+	return true
+}
+
+func Solve2() int {
+	lines := utils.GetLines("./day9/input.txt")
+
+	board := make(Board, len(lines))
+	for y := range board {
+		board[y] = []byte(lines[y])
+		for x := range board[y] {
+			board[y][x] -= '0'
+		}
+	}
+
+	var sizes []int
+	for y := range board {
+		for x := range board[y] {
+			if checkLow(board, x, y) > 0 {
+				count := 0
+
+				expand(&count, board, x, y)
+				sizes = append(sizes, count)
+			}
+		}
+	}
+
+	sort.Ints(sizes)
+
+	return sizes[len(sizes)-1] * sizes[len(sizes)-2] * sizes[len(sizes)-3]
+}
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/Olaroll/adventofcode21/day8"
+	"github.com/Olaroll/adventofcode21/day9"
 )
 
 func main() {
-	fmt.Println(day8.Solve2())
+	fmt.Println(day9.Solve2())
 }
